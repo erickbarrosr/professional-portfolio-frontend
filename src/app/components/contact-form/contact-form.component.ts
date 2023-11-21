4c3805180b329e11f2a37a73a96e00ddff1d15ba
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-21 17:13:33 -0300

    feat(contact-form.component.ts): get form fields and validations

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  @Input() sendButton!: string;
 
+  contactForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      id: new FormControl(''),
+      name: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name')!;
+  }
+
+  get lastName() {
+    return this.contactForm.get('lastName')!;
+  }
+
+  get email() {
+    return this.contactForm.get('email')!;
+  }
+
+  get message() {
+    return this.contactForm.get('message')!;
+  }
+
+  submit() {
+    if (this.contactForm.invalid) {
+      return;
+    }
+    console.log('Enviou o formulário...');
+  }
 }
