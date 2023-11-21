553bedb1f822dfadbbd6cf78683fac4cba311adb
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-21 17:08:03 -0300

    feat(contact.component.ts): add property 'sendButton'

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  sendButton: string = 'Enviar';
 }
