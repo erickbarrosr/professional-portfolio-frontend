10af3f718f00349c37c6d40c6a536cfc42cf6ed4
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-20 21:25:34 -0300

    feat(app.module.ts): imported contact page and contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, ContactComponent, ContactFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
