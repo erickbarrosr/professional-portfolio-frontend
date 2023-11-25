a819d21266c44077820a3a60c29eb0144e9a8995
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-25 17:29:30 -0300

    fix(app-routing.module.ts): fixed path route home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ContactComponent } from './components/pages/contact/contact.component';
 import { TechnologiesComponent } from './components/pages/technologies/technologies.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'technologies', component: TechnologiesComponent },
 ];
