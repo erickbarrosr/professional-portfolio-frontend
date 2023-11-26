380ee61efc51ceeab0e52d1f65ceddee75a98721
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-25 22:49:31 -0300

    feat(experiences.component.ts): created experiences component

---
+++ src/app/components/pages/experiences/experiences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.css']
+})
+export class ExperiencesComponent {
+
+}
