439bab92f79b1a1f9837febc84c8ae0184dd3e52
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-26 14:56:04 -0300

    feat(contact.component.ts): imported 'MessagesService' and 'Router'

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   sendButton: string = 'Enviar';
+
+  constructor(
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
 }
